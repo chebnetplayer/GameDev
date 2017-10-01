28638da97f27b5506ab28404fae4240e503928d3
Unknown <79196670853@ya.ru>
2017-10-01 03:06:49 +0300

    firstCommit

---
+++ first task/Assets/OpenDoorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoorTrigger : MonoBehaviour
+{
+    public GameObject door;
+    public Light light;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            door.transform.Rotate(0, -90, 0);
+            light.intensity = 5;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            door.transform.Rotate(0, 90, 0);
+            light.intensity = 0;
+        }
+    }
+}
---
+++ first task/Assets/PersonScript.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+
+public class PersonScript : MonoBehaviour
+{
+    public float PersonSpeed = 5;
+    public float PersonRotation = 200;
+    public GameObject Camera;
+    public Quaternion Rotation;
+
+    // Use this for  initialization
+    void Start()
+    {
+        Rotation = Camera.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * PersonRotation * Time.deltaTime, 0);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), (0-9.8f), Input.GetAxis("Vertical"));
+        movement *= Time.deltaTime*PersonSpeed;
+        movement = Vector3.ClampMagnitude(movement, PersonSpeed);
+        movement = transform.TransformDirection(movement);
+        GetComponent<CharacterController>().Move(movement);
+        /*if ((Camera.transform.rotation.eulerAngles.x > 310) || (Camera.transform.rotation.eulerAngles.x < 50))
+        {
+            Camera.transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * PersonRotation, 0, 0);
+        }
+        transform.Rotate(0, Input.GetAxis("Mouse X") * PersonRotation * Time.deltaTime, 0);
+        transform.Translate(Input.GetAxis("Horizontal") * PersonSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * PersonSpeed * Time.deltaTime);
+        if (!((Camera.transform.rotation.eulerAngles.x > 310) || (Camera.transform.rotation.eulerAngles.x < 50)))
+        {
+            Camera.transform.rotation = Rotation;
+        }
+        Rotation = Camera.transform.rotation;*/
+    }
+}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+ 
