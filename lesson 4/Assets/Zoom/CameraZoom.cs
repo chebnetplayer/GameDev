be31d307d0a2dd13f0490c4966ad88f1688bc2d3
Unknown <79196670853@ya.ru>
2017-10-23 19:39:42 +0300

    Rewrite ZoomScript

---
+++ lesson 4/Assets/Zoom/CameraZoom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraZoom : MonoBehaviour
+{
+    const string ToNormal= "ToNormal";
+    const string IsScrollingIn = "IsScrollingIn";
+    const string IsScrollingOut = "IsScrollingOut";
+    const string MouseScroll = "Mouse ScrollWheel";
+
+    void Start() {
+    }
+
+    void Update()
+    {        
+        if ((Input.GetAxis(MouseScroll) > 0))
+            ChangeParametersValue(IsScrollingIn, IsScrollingOut, ToNormal);
+        if ((Input.GetAxis(MouseScroll) < 0))
+            ChangeParametersValue(IsScrollingOut, IsScrollingIn, ToNormal);
+        if ((Input.GetAxis(MouseScroll) < 0) &&
+            GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("NormalIn"))
+        {
+            ChangeParametersValue(ToNormal, IsScrollingIn, IsScrollingOut);
+        }
+        if ((Input.GetAxis(MouseScroll) > 0) &&
+           GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("NormalOut"))
+        {
+            ChangeParametersValue(ToNormal,IsScrollingIn, IsScrollingOut);
+        }
+    }
+    void ChangeParametersValue(string trueparameter,string falseparameter1,string falseparameter2)
+    {
+        GetComponent<Animator>().SetBool(trueparameter, true);
+        GetComponent<Animator>().SetBool(falseparameter1, false);
+        GetComponent<Animator>().SetBool(falseparameter2, false);
+    }
+}
